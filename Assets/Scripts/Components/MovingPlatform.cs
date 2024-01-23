3fcec7a2b029d945f0e8d9a4c7fb61a378f0b233
f-fagua <91572042+f-fagua@users.noreply.github.com>
2024-01-23 16:55:52 -0500

    Moving Platforms (WIP)

---
+++ Assets/Scripts/Components/MovingPlatform.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[Serializable]
+public struct MovingPlatform : IComponentData
+{
+    public float3 TranslationAxis;
+    public float TranslationAmplitude;
+    public float TranslationSpeed;
+    public float3 RotationAxis;
+    public float RotationSpeed;
+
+    [HideInInspector] 
+    public bool IsInitialized;
+    [HideInInspector] 
+    public float3 OriginalPosition;
+    [HideInInspector]
+    public quaternion OriginalRotation;
+}
