f7e63a7eb9296b60d01a4bb68e4edf01f361a196
f-fagua <felipe.fagua@unity3d.com>
2024-01-24 11:00:03 -0500

    testing the weak loading

--- Assets/Samples/Character Controller/1.1.0-exp.10/Standard Characters/ThirdPerson/Scripts/ThirdPersonPlayerAuthoring.cs
+++ Assets/Samples/Character Controller/1.1.0-exp.10/Standard Characters/ThirdPerson/Scripts/ThirdPersonPlayerAuthoring.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using Unity.Entities;
+using Unity.Entities.Content;
+using Unity.Transforms;
 
 [DisallowMultipleComponent]
 public class ThirdPersonPlayerAuthoring : MonoBehaviour
 {
+    // Weak ref test
+    public WeakObjectReference<Mesh> mesh;
+    public WeakObjectReference<Material> material;
+
     public GameObject ControlledCharacter;
     public GameObject ControlledCamera;
 
@@ -17,7 +23,45 @@ public class ThirdPersonPlayerAuthoring : MonoBehaviour
                 ControlledCharacter = GetEntity(authoring.ControlledCharacter, TransformUsageFlags.Dynamic),
                 ControlledCamera = GetEntity(authoring.ControlledCamera, TransformUsageFlags.Dynamic),
             });
+            AddComponent(entity, new MeshComponentData{mesh = authoring.mesh, material = authoring.material});
             AddComponent<ThirdPersonPlayerInputs>(entity);
+        }
+    }
+}
+
+public struct MeshComponentData : IComponentData
+{
+    public bool startedLoad;
+    public WeakObjectReference<Mesh> mesh;
+    public WeakObjectReference<Material> material;
+}
+
+[WorldSystemFilter(WorldSystemFilterFlags.Default)]
+[UpdateInGroup(typeof(PresentationSystemGroup))]
+
+public partial struct RenderFromWeakObjectReferenceSystem : ISystem
+{
+    public void OnCreate(ref SystemState state) { }
+    public void OnDestroy(ref SystemState state) { }
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (var dec in SystemAPI.Query<RefRW<MeshComponentData>>())
+        {
+            Debug.Log($"mesh loading status: {dec.ValueRW.mesh.LoadingStatus}");
+            Debug.Log($"material loading status: {dec.ValueRW.material.LoadingStatus}");
+            
+            if (!dec.ValueRW.startedLoad)
+            {
+                dec.ValueRW.mesh.LoadAsync();
+                dec.ValueRW.material.LoadAsync();
+                dec.ValueRW.startedLoad = true;
+            }
+            if (dec.ValueRW.mesh.LoadingStatus == ObjectLoadingStatus.Completed &&
+                dec.ValueRW.material.LoadingStatus == ObjectLoadingStatus.Completed)
+            {
+                //Graphics.DrawMesh(dec.ValueRO.mesh.Result,
+                    //transform.ValueRO.Value, dec.ValueRO.material.Result, 0);
+            }
         }
     }
 }
